d501b3cefb15a7e0e1acbfd92f318a9e7645acc7
G-Bhavagna <gaddebhavagna1@gmail.com>
2024-06-15 12:01:05 +0530

    intial commit

---
+++ SJCC-MockHackathon/src/main/java/com/example/SJCC_MockHackathon/SjccMockHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.SJCC_MockHackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjccMockHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjccMockHackathonApplication.class, args);
+	}
+
+}
---
+++ SJCC-MockHackathon/src/test/java/com/example/SJCC_MockHackathon/SjccMockHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SJCC_MockHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjccMockHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
